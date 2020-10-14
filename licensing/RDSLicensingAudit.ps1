0636058e310188cc5c20f6814971531274990ee5
Chris Phillips <chris.phillips@quickthinkcloud.com>
2020-10-14 16:33:17 +0100

    Update RDSLicensingAudit.ps1

--- licensing/RDSLicensingAudit.ps1
+++ licensing/RDSLicensingAudit.ps1
@@ -712,11 +712,68 @@ Function Test-Cred {
         }
     }
 }
+Function UpdatesAvailable {
 
+    #check that the destination directory exists
+    if (!(Test-Path $updateDirectoryName)) {  
+        #CreateDirectory
+        New-Item -Name "$($updateDirectoryName)" -ItemType "directory"
+    }
+    
+    #check the latest update file exists
+    if (!(Test-Path "$($updateDirectoryName)\$($updatedVersionName)")) {
+        
+        #download the latest
+        Invoke-WebRequest $scriptSourceURL -OutFile "$($updateDirectoryName)\$($updatedVersionName)"
+    }
+
+} # End Function
+Function Update-Myself {
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true,
+                   Position = 0)]
+        [string]$SourcePath
+    )
+    #check that the destination file exists
+    if (Test-Path $SourcePath)
+    {
+    #The path of THIS script
+    $CurrentScript = $MyInvocation.ScriptName
+        if (!($SourcePath -eq $CurrentScript ))
+        {
+            if ($(Get-Item $SourcePath).LastWriteTimeUtc -gt $(Get-Item $CurrentScript ).LastWriteTimeUtc)
+            {
+                write-host "Updating..."
+                Copy-Item $SourcePath $CurrentScript -Force
+                #If the script was updated, run it with orginal parameters
+                #&$CurrentScript $script:args
+                &$CurrentScript $ConfigFile
+                exit
+            }
+        }
+    }
+    write-host "No update required"
+    Remove-Item "$($updateDirectoryName)" -Recurse -Force -Confirm:$false
+} # End Function
 
 ### SCRIPT BODY ###
 #Run the config .ps1 to set the variables
 . .\$ConfigFile
+
+
+### SELF UPDATER SECTION ###
+#SCRIPT ADMIN VARIABLES!
+$scriptName = "RESLicensingAudit.ps1"
+$updateDirectoryName = "RDSLUAUpdates"
+$updatedVersionName = "RDSLUA_latest.ps1"
+$scriptSourceURL = "https://raw.githubusercontent.com/quickthinkcloud/public/master/licensing/RESLicensingAudit.ps1"
+
+UpdatesAvailable
+Update-Myself "$($updateDirectoryName)\$($updatedVersionName)"
+### END OF SELF UPDATER SECTION ###
+
 
 cd $workingDir
 
