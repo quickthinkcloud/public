e26a8ce59426ca6da64b7e3735b398c043856d02
Chris Phillips <chris.phillips@quickthinkcloud.com>
2020-12-01 16:31:03 +0000

    Update Base_Server_2016.ps1

--- boxstarter/Base_Server_2016.ps1
+++ boxstarter/Base_Server_2016.ps1
@@ -3,8 +3,8 @@ inetcpl.cpl
 $winLogonKey="HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 Remove-ItemProperty -Path $winLogonKey -Name "AWSAccessKey" -ErrorAction SilentlyContinue
 Remove-ItemProperty -Path $winLogonKey -Name "AWSSecretKey" -ErrorAction SilentlyContinue
-New-ItemProperty -Path $winLogonKey -Name "AWSAccessKey" -Value "AKIAJXMF3GE4CVZALHWA" -ErrorAction SilentlyContinue 
-New-ItemProperty -Path $winLogonKey -Name "AWSSecretKey" -Value "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" -ErrorAction SilentlyContinue 
+New-ItemProperty -Path $winLogonKey -Name "AWSAccessKey" -Value "a" -ErrorAction SilentlyContinue 
+New-ItemProperty -Path $winLogonKey -Name "AWSSecretKey" -Value "a/a" -ErrorAction SilentlyContinue 
 START http://boxstarter.org/package/url?https://raw.githubusercontent.com/quickthinkcloud/public/master/boxstarter/Base_Server_2016.ps1
 #>
 
@@ -59,7 +59,7 @@ Function MSIDownloadAndInstall {
 $Boxstarter.RebootOk=$true # Allow reboots?
 $Boxstarter.NoPassword=$false # Is this a machine with no login password?
 $Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
-$useAWSRepository = $false
+$useAWSRepository = $true
 
 
 # Basic setup
@@ -162,10 +162,6 @@ If ($useAWSRepository -eq $true) {
     # Read-S3Object -BucketName qtcsoftwarerepo -Key LibreOffice/LibreOffice_5.3.6_Win_x64.msi -File C:\Repository\LibreOffice_5.3.6_Win_x64.msi -Region eu-west-1
     
 } # End If ($useAWSRepository -eq $true)
-
-
-    #AWS Configuration
-    Initialize-AWSDefaultConfiguration -AccessKey "AKIAJXMF3GE4CVZALHWA" -SecretKey "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" -Region eu-west-1
 
 
 #Essential Software
