1dd2fad6a29462805d4c1b8b06bbf2c1f73124a1
Chris Phillips <chris.phillips@quickthinkcloud.com>
2024-10-01 12:41:38 +0100

    Update RDSLicensingAudit.ps1

--- licensing/RDSLicensingAudit.ps1
+++ licensing/RDSLicensingAudit.ps1
@@ -27,7 +27,7 @@ param (
 )
 ### END OF PARAMETERS ###
 
-$scriptVersion = 20240920
+$scriptVersion = 20241001-1240
 
 $proceed = $false
 $daysOfMonthToAudit = @(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31)
@@ -1026,6 +1026,7 @@ If ($proceed) {
         $i++
     } #End foreach
 
+    Write-Host "Waiting 2 minutes after credentials check..." -ForegroundColor Yellow
     Start-Sleep -Seconds 120
 
 
